18ace28a0e85810a8b1cce93f24f54a0c579fd9a
Jaris88 <jaris88.fedorv@gmail.com>
2023-03-18 16:50:27 +0300

    1

---
+++ messiGoals.js
@@ -0,0 +1,7 @@
+// Посчитать все голы Месси втрех соревнованиях
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // Создаем переменную равную трем значениям функции.
+    let res = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    // Вызываем результат через
+    return(res);
+}
